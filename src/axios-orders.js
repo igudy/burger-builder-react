1499a35ac492dcfb58b8fb1f143ac72d362595f5
igudy <goodnessigunma1@gmail.com>
2022-08-31 01:35:00 -0700

    Deployed on firebase

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-burger.firebaseio.com/'
+    baseURL: 'https://react-my-burger-40b37-default-rtdb.firebaseio.com/'
 });
-
 export default instance;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,11 +48,11 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAH1tDmV2KUiIoEHMupaeOZGyQTMsdMAIA';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:verifyPassword?key=AIzaSyAH1tDmV2KUiIoEHMupaeOZGyQTMsdMAIA';
         }
-        axios.post(url, authData)
+       axios.post(url, authData)
             .then(response => {
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
